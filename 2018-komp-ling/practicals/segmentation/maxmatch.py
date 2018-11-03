cb0cc4a2b0beb2aa6e698f3614172e809fc062b7
Marina <34004576+kak-to-tak@users.noreply.github.com>
2018-11-03 20:48:29 +0300

    Add files via upload

---
+++ 2018-komp-ling/practicals/segmentation/maxmatch.py
@@ -0,0 +1,16 @@
+﻿import sys
+
+with open(sys.argv[1])  as f:
+    dictionary = sorted([l.strip() for l in f], key=len, reverse=True)
+
+def tokenize(sentence):
+    if not len(sentence)==0:
+        try:
+            word = next(w for w in dictionary if sentence.startswith(w))
+        except StopIteration:
+            word = sentence[0]
+        return word + tokenize(sentence[len(word):])
+
+for sentence in sys.stdin:
+    for word in tokenize(sentence):
+        print(word)
