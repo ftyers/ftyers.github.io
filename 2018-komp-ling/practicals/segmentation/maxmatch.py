148e1508b040e494144e07f2074f50411160f7a9
Marina <34004576+kak-to-tak@users.noreply.github.com>
2018-11-03 20:54:56 +0300

    Update maxmatch.py

--- 2018-komp-ling/practicals/segmentation/maxmatch.py
+++ 2018-komp-ling/practicals/segmentation/maxmatch.py
@@ -1,4 +1,9 @@
-﻿import sys
+"""
+This code sorts the dictionary to have the longest word as a first one and the shortest as the last 
+and separates the sentence when it sees the first word in the dectionary that matches the beginning of the sentence
+"""
+
+import sys
 
 with open(sys.argv[1])  as f:
     dictionary = sorted([l.strip() for l in f], key=len, reverse=True)
