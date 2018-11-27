52401a2cf8783a5b69df29107bd4f4a4868fc2b0
rysshe <rysshebeiyezs@gmail.com>
2018-11-27 13:00:59 +0300

    updated

--- 2018-komp-ling/quizzes/pluralization.py
+++ 2018-komp-ling/quizzes/pluralization.py
@@ -1,32 +1,14 @@
 import sys
-
-exeprions = {'man':'men', 'person':'people', 'woman':'women', 'mouse':'mice',
-             'foot':'feet', 'child': 'children', 'tooth':'teeth', 'goose':'geese', 'ox':'oxen'}
-f_exeptions = ['roof', 'belief', 'chef', 'chief']
-no_changes_exeptions = ['sheep', 'series', 'species', 'deer', 'fish', 'moose', 'salmon',\
-                        'aircraft', 'trout', 'means']
+import re
 
 word = sys.argv[1]
+plur = {'s<PL>': 'ses', 'sh<PL>': 'shes', 'ch<PL>': 'ches', 'tz<PL>': 'tzes', 'x<PL>': 'xes'}
 
-if word in exeprions:
-    print(exeprions[word])
-elif word.endswith('is'):
-    print(word[:-2] + 'es')
-elif word.endswith('us'):
-    print(word[:-2] + 'i')
-elif word.endswith('s' or 'ss' or 'sh' or 'ch' or 'tch' or 'z' or 'x'):
-    print(word + 'es')
-elif word not in f_exeptions and word.endswith('f' or 'fe'):
-    print(word[:-1] + 'ves')
-elif word in no_changes_exeptions:
-    print(word)
-elif word.endswith('ay' or 'ey' or 'oy'):
-    print(word + 's')
-elif word.endswith('y'):
-    print(word[:-1] + 'ies')
-elif word.endswith('a'):
-    print(word[:-1] + 'ae')
-elif word.endswith('um'):
-    print(word[:-2] + 'a')
-else:
-    print(word, 's')
+for key, value in plur:
+    if word.endswith(key):
+        word = word.replace(key, value)
+    word = re.sub('<PL>', 's', word)
+
+print(word)
+
+
